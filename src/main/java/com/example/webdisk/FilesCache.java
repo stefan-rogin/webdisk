8a07cfe2cfd3f8829fe48eb799785d46b01d4326
github-stefan-rogin <stefan.rogin@teads.tv>
2025-02-14 18:25:12 +0200

    refactor

--- src/main/java/com/example/webdisk/FilesCache.java
+++ src/main/java/com/example/webdisk/FilesCache.java
@@ -2,9 +2,10 @@ package com.example.webdisk;
 
 import java.util.HashSet;
 import java.util.Set;
-import java.util.random.RandomGenerator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.stereotype.Service;
 
@@ -22,7 +23,10 @@ public class FilesCache {
     }
 
     public String newFile() {
-        
+        return Stream.generate(new FilesNameSupplier())
+            .filter(fileName -> !this.files.contains(fileName))
+            .limit(1)
+            .collect(Collectors.joining());
     }
 
     public String[] findFilesForPattern(String pattern) {
@@ -37,23 +41,5 @@ public class FilesCache {
 
     public int getSize() {
         return this.files.size();
-    }
-
-    private String generateFileName() {
-        // 0-9: 48-57
-        // a-z: 97-122
-        // A-Z: 65-90
-        // -_: 45,95
-        final int LIMIT_ASCII_LOW = 45, LIMIT_ASCII_HIGH = 123, MIN_FILENAME_LENGTH = 1, MAX_FILENAME_LENGTH = 64;
-
-        RandomGenerator generator = RandomGenerator.of("L128X256MixRandom");
-        int randomFileNameLength = generator.nextInt(MIN_FILENAME_LENGTH, MAX_FILENAME_LENGTH + 1);
-        String randomFileName = generator.ints(LIMIT_ASCII_LOW, LIMIT_ASCII_HIGH)
-                .filter(n -> n == 45 || (n >= 48 && n <= 57) || (n >= 65 && n <= 90) || n == 95
-                        || (n >= 97 && n <= 122))
-                .limit(randomFileNameLength)
-                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-                .toString();
-        return randomFileName;
     }
 }
