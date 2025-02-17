f349fd4e7d7302f0a1c2c2569e57a8023aac5fc5
github-stefan-rogin <stefan.rogin@teads.tv>
2025-02-17 20:17:48 +0200

    pain

---
+++ src/test/java/com/example/webdisk/FilesAccessTest.java
@@ -0,0 +1,104 @@
+package com.example.webdisk;
+
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Arrays;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Stream;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(MockitoExtension.class)
+public class FilesAccessTest {
+
+    @Mock
+    private TaskExecutor taskExecutor;
+
+    @Mock
+    private Path pathMock;
+
+    @InjectMocks
+    private FilesAccess filesAccess;
+
+    @BeforeEach
+    public void setup() {
+        filesAccess.setPath("/mock");
+    }
+
+    @Test
+    public void shouldListStorageAndFilterValidFiles() throws IOException {
+        Path one = mock(Path.class);      // Valid
+        Path andone = mock(Path.class);   // Valid
+        Path not1 = mock(Path.class);     // Not valid
+        Path twodots = mock(Path.class);  // Not valid
+        Stream<Path> pathStream = Stream.of(one, andone);
+
+        when(Files.list(any(Path.class))).thenReturn(pathStream);
+        when(one.getFileName()).thenReturn(Path.of("one"));
+        when(andone.getFileName()).thenReturn(Path.of("andone"));
+        when(not1.getFileName()).thenReturn(Path.of("not1.tmp"));
+        when(Files.isDirectory(one)).thenReturn(false);
+        when(Files.isDirectory(andone)).thenReturn(false);
+        when(Files.isDirectory(not1)).thenReturn(false);
+        when(Files.isDirectory(twodots)).thenReturn(true);
+
+        assertThat(filesAccess.listFiles()).isEqualTo(Arrays.asList("one", "andone"));
+    }
+
+    @Test
+    public void shouldGetFileContentsInAStream() throws IOException {
+        InputStream inputStreamMock = new ByteArrayInputStream("testContent".getBytes());
+        MockedStatic<Files> filesStaticMock = Mockito.mockStatic(Files.class);
+
+        filesStaticMock.when(() -> Files.newInputStream(any(Path.class))).thenReturn(inputStreamMock);
+
+        assertThat(filesAccess.getFile("any")).isEqualTo(inputStreamMock);
+    }
+
+    @Test
+    public void shouldGetFileContentsInAStreamAsync() throws Exception {
+        InputStream inputStreamMock = new ByteArrayInputStream("testContent".getBytes());
+        MockedStatic<Files> filesStaticMock = Mockito.mockStatic(Files.class);
+
+        filesStaticMock.when(() -> Files.newInputStream(any(Path.class))).thenReturn(inputStreamMock);
+
+        assertThat(filesAccess.getFileAsync("any").get()).isEqualTo(inputStreamMock);
+    }
+
+    @Test
+    public void shouldStoreFile() throws IOException {
+        MultipartFile multipartFileMock = mock(MultipartFile.class);
+        InputStream inputStreamMock = new ByteArrayInputStream("testContent".getBytes());
+        MockedStatic<Files> filesStaticMock = Mockito.mockStatic(Files.class);
+        AtomicBoolean fileCopied = new AtomicBoolean(false);
+
+        when(multipartFileMock.getInputStream()).thenReturn(inputStreamMock);
+        filesStaticMock.when(() -> Files.copy(any(InputStream.class), any(Path.class), any(StandardCopyOption.class)))
+                .thenAnswer(i -> {
+                    fileCopied.set(true);
+                    return 1L;
+                });
+
+        filesAccess.putFile("testFile", multipartFileMock);
+
+        assertThat(fileCopied).isTrue();
+    }
+}
--- src/test/java/com/example/webdisk/FilesControllerTests.java
+++ src/test/java/com/example/webdisk/FilesControllerTests.java
@@ -1,25 +1,33 @@
 package com.example.webdisk;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.InputStream;
+import java.util.concurrent.CompletableFuture;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
 
 @AutoConfigureMockMvc
 @SpringBootTest
 class FilesControllerTests {
 
-    @Autowired
     private MockMvc mockMvc; 
 
     @Mock
@@ -28,12 +36,36 @@ class FilesControllerTests {
     @InjectMocks
     private FilesController controller;
 
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
     @Test
-    void contextLoads() throws Exception {
-        this.mockMvc.perform(get("/files/size")).andDo(print())
-            .andExpect(status().isOk())
-            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-            .andExpect(jsonPath("$.size").exists());
+    public void testGetFile() throws Exception {
+        String fileName = "testone";
+        InputStream inputStream = new ByteArrayInputStream("test_content".getBytes());
+        CompletableFuture<InputStream> futureInputStream = CompletableFuture.completedFuture(inputStream);
+        when(storage.getFileAsync(anyString())).thenReturn(futureInputStream);
+
+        CompletableFuture<ResponseEntity<InputStreamResource>> responseFuture = controller.getFileForFileName(fileName, null);
+        ResponseEntity<InputStreamResource> response = responseFuture.get();
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(200));
+        assertThat(response.getHeaders().getFirst(HttpHeaders.CONTENT_DISPOSITION)).isEqualTo("attachment; filename=" + fileName);
+        assertThat(response.getBody()).isNotNull();
+    }
+    
+    @Test
+    public void testGetFileForFileName_NotFound() throws Exception {
+        String fileName = "none";
+        CompletableFuture<InputStream> futureInputStream = CompletableFuture.failedFuture(new IOException("File not found"));
+        when(storage.getFileAsync(anyString())).thenReturn(futureInputStream);
+
+        CompletableFuture<ResponseEntity<InputStreamResource>> responseFuture = controller.getFileForFileName(fileName, null);
+        ResponseEntity<InputStreamResource> response = responseFuture.get();
+
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(500));
     }
     
 }
