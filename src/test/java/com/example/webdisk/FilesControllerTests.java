7e859ba5ff5428da3a0049bf804daa48b15e8672
github-stefan-rogin <stefan.rogin@teads.tv>
2025-02-18 21:02:39 +0200

    wip

--- src/test/java/com/example/webdisk/FilesControllerTests.java
+++ src/test/java/com/example/webdisk/FilesControllerTests.java
@@ -1,84 +1,131 @@
 package com.example.webdisk;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.test.web.servlet.MvcResult;
 
+import com.example.webdisk.response.FilesPostFileResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.head;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.head;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.util.Map;
-import java.util.regex.Pattern;
 
 @AutoConfigureMockMvc
 @SpringBootTest
 class FilesControllerTests {
 
     @Autowired
-    private MockMvc mockMvc; 
-
-    @Mock
-    private FilesAccess storage;
-
-    @InjectMocks
-    private FilesController controller;
+    private MockMvc mockMvc;
 
     @Test
     void getFilesSizeTests() throws Exception {
-        this.mockMvc.perform(get("/files/size"))
-            .andExpect(status().isOk())
-            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-            .andExpect(jsonPath("$.size").value(7));
+        mockMvc.perform(get("/files/size"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.size").exists());
     }
-    
+
     @Test
     void getFileForFileNameTests() throws Exception {
-        this.mockMvc.perform(get("/files/one"))
-            .andExpect(status().isOk())
-            .andExpect(header().string("Content-disposition", "attachment; filename=one"))
-            .andExpect(content().contentType(MediaType.APPLICATION_OCTET_STREAM));
+        mockMvc.perform(get("/files/one"))
+                .andExpect(status().isOk())
+                .andExpect(header().string("Content-disposition", "attachment; filename=one"))
+                .andExpect(content().contentType(MediaType.APPLICATION_OCTET_STREAM))
+                .andExpect(content().string("one"));
     }
 
     @Test
-    void postFileTests() throws Exception {
-        MockMultipartFile file = new MockMultipartFile("file", "oneup", 
-                MediaType.TEXT_PLAIN_VALUE, "one".getBytes());
-
-        this.mockMvc.perform(get("/files/size"))
-            .andExpect(jsonPath("$.size").value(12));
-        this.mockMvc.perform(multipart("/files/upload")
-            .file(file))
-            .andExpect(status().isOk())
-            .andExpect(result -> {
-                String response = result.getResponse().getContentAsString();
-                ObjectMapper objectMapper = new ObjectMapper();
-                Map<String, String> responseMap = objectMapper.readValue(response, Map.class);
-                String fileName = responseMap.get("filename");
-                assertTrue(fileName).matches("^[a-zA-Z0-9-_]{1,64}$");
-
-                Pattern namePattern = Pattern.compile("[a-zA-Z0-9-_]{1,64}");
-                assertTrue(namePattern.matcher(response).find());
-            });
-        this.mockMvc.perform(get("/files/size"))
-            .andExpect(jsonPath("$.size").value(13));
+    void getFileForFileNameShouldReturnNotFoundForFileNotInWebDisk() throws Exception {
+        mockMvc.perform(get("/files/none"))
+                .andExpect(status().isNotFound());
     }
-    
+
+    @Test
+    void getFileForFileNameShouldReturnNoContentForHeadRequest() throws Exception {
+        mockMvc.perform(head("/files/one"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(""));
+    }
+
+    @Test
+    void postAndDeleteFileTests() throws Exception {
+        MockMultipartFile file = new MockMultipartFile("file", "oneup",
+                MediaType.TEXT_PLAIN_VALUE, "oneup".getBytes());
+
+        mockMvc.perform(get("/files/size"))
+                .andExpect(jsonPath("$.size").value(7));
+        MvcResult result = mockMvc.perform(multipart("/files/upload")
+                .file(file))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        ObjectMapper mapper = new ObjectMapper();
+        String responseContent = result.getResponse().getContentAsString();
+        FilesPostFileResponse newFileResponse = mapper.readValue(responseContent, FilesPostFileResponse.class);
+        String newFileName = newFileResponse.fileName();
+        mockMvc.perform(get("/files/" + newFileName))
+                .andExpect(status().isOk())
+                .andExpect(header().string("Content-disposition", "attachment; filename=" + newFileName))
+                .andExpect(content().contentType(MediaType.APPLICATION_OCTET_STREAM))
+                .andExpect(content().string("oneup"));
+        // Clean
+        mockMvc.perform(delete("/files/" + newFileName))
+                .andExpect(status().isOk());
+        mockMvc.perform(get("/files/size"))
+                .andExpect(jsonPath("$.size").value(7));
+
+    }
+
+    @Test
+    void putAndDeleteFileTests() throws Exception {
+        MockMultipartFile file = new MockMultipartFile("file", "oneone",
+                MediaType.TEXT_PLAIN_VALUE, "two".getBytes());
+        mockMvc.perform(multipart("/files/oneone")
+                .file(file)
+                .with(request -> {
+                    request.setMethod("PUT");
+                    return request;
+                }))
+                .andExpect(status().isOk());
+        mockMvc.perform(get("/files/oneone"))
+                .andExpect(content().string("two"));
+        // Clean
+        mockMvc.perform(delete("/files/oneone"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void putFileShouldValidateFileNames() throws Exception {
+        MockMultipartFile file = new MockMultipartFile("file", "one.one",
+                MediaType.TEXT_PLAIN_VALUE, "two".getBytes());
+        mockMvc.perform(multipart("/files/one.one")
+                .file(file)
+                .with(request -> {
+                    request.setMethod("PUT");
+                    return request;
+                }))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void searchFileTests() throws Exception {
+        mockMvc.perform(get("/files/search?pattern=one"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.results.length()").value(2));
+        mockMvc.perform(get("/files/search?pattern=none"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.results.length()").value(0));
+    }
+
 }
